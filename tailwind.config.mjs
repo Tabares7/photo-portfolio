e204e57ae26c39a4711d5d5505dc6d591eb761a3
tabares7 <pedrotabares777@gmail.com>
2024-01-17 15:51:07 -0600

    Add Astro types reference and new dependencies Update index page layout and add header component Update tailwind.config.mjs and add new font and colors Update Layout.astro and add font and script tags

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			backgroundImage: {
-				'header': "url('src/assets/header-img.jpg')",
-			  },
-		},
-	},
-	plugins: [],
-}
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        header: "url('src/assets/header-img.jpg')",
+      },
+      colors: {
+        "main-black": "#020202",
+      },
+    },
+  },
+  plugins: [require("flowbite/plugin")],
+};
